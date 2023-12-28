e28a41779cd110e999fd7554f03b3a33a290c4e4
derekking <derek@hyperlounge.com>
2023-12-28 16:54:12 +0000

    Poly synth beta

--- js/PolySynth.js
+++ js/PolySynth.js
@@ -356,12 +356,14 @@ export default class PolySynth extends ModularSynth {
             this._libraryRoot.id = 'library-root';
             this._libraryRoot.className = 'library-root';
             this._libraryRoot.style.left = target.offsetLeft + 'px';
-            this._libraryRoot.style.top = (target.offsetTop + target.offsetHeight) + 'px';
+            const top = (target.offsetTop + target.offsetHeight);
+            this._libraryRoot.style.top = top + 'px';
+            this._libraryRoot.style.maxHeight = (window.innerHeight - top) + 'px';
             document.body.appendChild(this._libraryRoot);
             const libraryView = new LibraryView(this._libraryRoot.id, this._library, '');
             libraryView.addEventListener('preset-selected', evt => {
                 this._libraryRoot.style.display = 'none';
-                this.loadPatchFromLibrary(evt.detail).then(patch => {
+                this.loadPresetFromLibrary(evt.detail).then(patch => {
                     this.patch = patch;
                     this._presetId = evt.detail;
                     if (location.search) {
@@ -374,7 +376,7 @@ export default class PolySynth extends ModularSynth {
         }
     }
 
-    loadPatchFromLibrary(id) {
+    loadPresetFromLibrary(id) {
         return new Promise(resolve => {
             const filePath = this._library.getPresetPathById(id);
             const xhr = new XMLHttpRequest();
@@ -448,7 +450,7 @@ export default class PolySynth extends ModularSynth {
         if (params.preset) {
             const preset = this._library.getPresetByNameAndBank(params.preset, params.bank);
             this._presetId = preset.id;
-            this.loadPatchFromLibrary(preset.id).then(patch => {
+            this.loadPresetFromLibrary(preset.id).then(patch => {
                 this.patch = patch;
                 if (params.changes) {
                     this._patch.set(JSON.parse(params.changes));
@@ -495,7 +497,7 @@ export default class PolySynth extends ModularSynth {
 
     getPatchChanges() {
         return new Promise(resolve => {
-            this.loadPatchFromLibrary(this._presetId).then(patch => {
+            this.loadPresetFromLibrary(this._presetId).then(patch => {
                 const preset = patch;
                 const currentPatch = this.patch;
                 const changes = {};

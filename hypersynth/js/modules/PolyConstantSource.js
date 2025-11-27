cc9c2c5d6c7ece950bfd71f3d453992062f6d3a0
Derek King <derek@hyperlounge.com>
2025-11-27 17:20:02 +0000

    Updated hypersynth

--- hypersynth/js/modules/PolyConstantSource.js
+++
@@ -1,9 +0,0 @@
-import PolyAudioNode from './PolyAudioNode.js';
-
-export default class PolyConstantSource extends PolyAudioNode {
-    get _AudioNodeClass() {return ConstantSourceNode}
-
-    get offset() {
-        return this.getPolyParam('offset');
-    }
-}
--- hypersynth/js/modules/PolyGain.js
+++
@@ -1,9 +0,0 @@
-import PolyAudioNode from './PolyAudioNode.js';
-
-export default class PolyGain extends PolyAudioNode {
-    get _AudioNodeClass() { return GainNode }
-
-    get gain() {
-        return this.getPolyParam('gain');
-    }
-}
